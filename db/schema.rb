ea043710495eb4146ea72448c79785c8f885aa5c
decatan <ys@thecompany.sg>
2014-03-28 23:40:40 +0800

    Made a basic user model without password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328094924) do
+ActiveRecord::Schema.define(version: 20140328153619) do
 
   create_table "tasks", force: true do |t|
     t.string   "title"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20140328094924) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
