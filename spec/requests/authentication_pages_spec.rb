2396eef605fabe7d2bd870d641ea8eeedad638ab
decatan <ys@thecompany.sg>
2014-03-30 22:33:27 +0800

    trying to update the rspec pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,9 +10,10 @@ describe "AuthenticationPages" do
   	it { should have_content('Sign in') }
 
   	describe "with invalid information" do
-  		before { click_button "Sign in" }
+  		before { click_button "Sign In" }
 
-  		it { should have_selector('div.alert.alert-error') }
+  		it { should have_error_message('Invalid') }
+      it { should have_title('Sign in') }
 
   		describe "after visiting another page" do
   			before { click_link "Home" }
@@ -25,16 +26,16 @@ describe "AuthenticationPages" do
   		before do
   			fill_in "Email",    with: user.email.upcase
   			fill_in "Password", with: user.password
-  			click_button "Sign in"
+  			click_button "Sign In"
   		end
 
   		it { should have_link('Profile',     href: user_path(user)) }
   		it { should have_link('Sign out',    href: signout_path) }
-  		it { should_not have_link('Sign in', href: signin_path) }
+  		it { should_not have_link('Sign In', href: signin_path) }
 
       describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        it { should have_link('Sign In') }
       end
   	end
   end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,24 +6,24 @@ subject { page }
   describe "Home page" do
   	before { visit root_path }
 
-    it { should have_content('This is our home page.') }
+    it { should have_title('Home') }
   end
 
   describe "About page" do
   	before { visit about_path }
 
-    it { should have_content('We are The Company') }
+    it { should have_title('About us') }
   end  
 
   describe "Help page" do
   	before { visit help_path }
 
-  	it { should have_content('This is our help page') }
+  	it { should have_title('Help') }
   end
 
   describe "Contact page" do
   	before { visit contact_path }
 
-  	it { should have_content('This is our contact page')}
+  	it { should have_title('Contact us')}
   end
 end
